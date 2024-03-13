b28a76eefa0a0c56983b8c256e97ef23f34ae5a7
sofiacottone <cottonesofia@outlook.it>
2024-03-13 15:06:52 +0100

    feat: add js logic

--- for/js/script.js
+++ for/js/script.js
@@ -10,10 +10,6 @@
 //     'Piatti pronti'
 // ];
 // Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
-// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
-// Consigli:
-// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
-// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 
 const list = [
     'Latte',
