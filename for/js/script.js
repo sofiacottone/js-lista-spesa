1389bffa5fa928250fd43b3f08a7897ed2168e95
sofiacottone <cottonesofia@outlook.it>
2024-03-13 14:48:19 +0100

    init method for

---
+++ for/js/script.js
@@ -0,0 +1,17 @@
+// METODO FOR
+// Consegna:
+// Data una lista della spesa:
+// const list = [
+//     'Latte',
+//     'Cacao',
+//     'Insalata',
+//     'Carote',
+//     'Pomodori',
+//     'Piatti pronti'
+// ];
+// Stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente.
+// Svolgete questo esercizio in 2 versioni: col ciclo for e col ciclo while.
+// Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
+
