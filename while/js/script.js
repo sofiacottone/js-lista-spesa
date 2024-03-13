6417566f5767f38f6c2d016e57805f2f10d0e91f
sofiacottone <cottonesofia@outlook.it>
2024-03-13 15:41:37 +0100

    feat: add js logic

--- while/js/script.js
+++ while/js/script.js
@@ -15,3 +15,25 @@
 // - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ciclo while
 // - Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 
+const list = [
+    'Latte',
+    'Cacao',
+    'Insalata',
+    'Carote',
+    'Pomodori',
+    'Piatti pronti'
+];
+console.log(list);
+
+// prendo il contenitore della lista dal DOM
+const domList = document.querySelector('.list');
+
+let i = 0;
+// scorro l'elenco della lista finché l'indice dell'elemento è minore alla lunghezza dell'array
+while (i < list.length) {
+    const thisElement = list[i];
+
+    // stampo tutti gli elementi nel DOM
+    domList.innerHTML += `<li>${thisElement}</li>`;
+    i++;
+}
